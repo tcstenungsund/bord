980377041dd9901f2b70fda55a1864b36fc8b598
Pontus Olsson <58231866+pontusolsson03@users.noreply.github.com>
2022-12-05 09:26:22 +0100

    Försök här med

--- request.js
+++ request.js
@@ -1,5 +1,5 @@
 const fruitBtn = document.getElementById("fruit-btn");
-const fruitURL = "http://creepy-headscarf-hen.cyclic.app/fruit";
+const fruitURL = "https://creepy-headscarf-hen.cyclic.app/fruit";
 
 fruitBtn.addEventListener("click", function () {
   console.log("Fruit-knapp klickad");
