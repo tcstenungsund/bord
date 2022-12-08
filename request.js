eefc27c0dbf22da58775be9b48a3cc8a16ebbd0a
williamhansen03 <91659748+williamhansen03@users.noreply.github.com>
2022-12-08 11:45:10 +0100

    Fetches from API (#64) (#65)
    Co-authored-by: Pontus Olsson <58231866+pontusolsson03@users.noreply.github.com>

--- request.js
+++ request.js
@@ -1,13 +1,22 @@
 const fruitBtn = document.getElementById("fruit-btn");
-const fruitURL = "http://creepy-headscarf-hen.cyclic.app/fruit";
+const fruitUrl = "https://creepy-headscarf-hen.cyclic.app/fruit";
+const exUrl = "https://jsonplaceholder.typicode.com/todos/1";
+const contentEl = document.getElementById("api-content");
 
 fruitBtn.addEventListener("click", function () {
   console.log("Fruit-knapp klickad");
-  fetch(fruitURL)
+  fetch(fruitUrl, {
+    credentials: "include",
+    headers: {
+      "Content-Type": "text/html",
+    },
+  })
     .then((data) => {
-      return data.json();
+      return data.text();
     })
     .then((res) => {
       console.log(res);
+      console.log(typeof res);
+      contentEl.innerHTML = res;
     });
 });
