e51cd6e4417fe68553478d4728251f2e5a969f96
Pontus Olsson <58231866+pontusolsson03@users.noreply.github.com>
2022-12-11 00:14:25 +0100

    Able to send put-requests to API (#66)

--- request.js
+++ request.js
@@ -1,12 +1,13 @@
-const fruitBtn = document.getElementById("fruit-btn");
-const fruitUrl = "https://creepy-headscarf-hen.cyclic.app/fruit";
-const exUrl = "https://jsonplaceholder.typicode.com/todos/1";
 const contentEl = document.getElementById("api-content");
+const fruitBtn = document.getElementById("fruit-btn");
+
+const fruitUrl = "https://creepy-headscarf-hen.cyclic.app/fruit";
+const localhost = "http://localhost:8080/fruit";
 
 fruitBtn.addEventListener("click", function () {
   console.log("Fruit-knapp klickad");
-  fetch(fruitUrl, {
-    credentials: "include",
+  fetch(localhost, {
+    method: "GET",
     headers: {
       "Content-Type": "text/html",
     },
@@ -15,8 +16,49 @@ fruitBtn.addEventListener("click", function () {
       return data.text();
     })
     .then((res) => {
-      console.log(res);
-      console.log(typeof res);
       contentEl.innerHTML = res;
     });
+});
+
+const putBtn = document.getElementById("put-btn");
+putBtn.addEventListener("click", function () {
+  fetch(localhost, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      card_type: "primary_card",
+      card_id: "ABC123",
+      page_name: "apples",
+    }),
+  });
+});
+
+const sendPutBtn = document.getElementById("send-btn");
+sendPutBtn.addEventListener("click", function putInput() {
+  const pageInput = document.getElementById("page-input").value;
+  const idInput = document.getElementById("id-input").value;
+  const isPrimary = document.getElementById("is-primary").checked;
+  if (isPrimary) {
+    var cardType = "primary_card";
+  } else {
+    var cardType = "secondary_card";
+  }
+  console.log("🚀 ~ putInput ~ pageInput", pageInput);
+  console.log("🚀 ~ putInput ~ idInput", idInput);
+  console.log("🚀 ~ putInput ~ cardType", cardType);
+
+  fetch(localhost, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      card_type: cardType,
+      page_name: pageInput,
+      card_id: idInput,
+    }),
+  });
+  return;
 });
