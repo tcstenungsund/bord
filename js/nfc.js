2c3b6a49b64415b5c0822b0a59af3b34088643cf
williamhansen03 <william.hansen03@skola.stenungsund.se>
2022-12-16 12:35:51 +0100

    Layout under sidor air

--- js/nfc.js
+++ js/nfc.js
@@ -75,10 +75,21 @@ function workerMessage() {
   });
 }
 
+const layoutAir = document.querySelector("#layout-air");
 
 const btn = document.querySelector(".settings-btn");
 btn.addEventListener("click", function(){
   document.querySelector("#layout-start").style.display = "none";
-  document.querySelector("#layout-air").style.display = "block";
+  layoutAir.style.display = "block";
 
 });
+
+if(layoutAir.style.display != "none"){
+  layoutAir.addEventListener("click", function(){
+    document.querySelector(".image").style.display = "none";
+    document.querySelector(".image-2").style.display = "block";
+    document.querySelector(".text-box").style.display = "block";
+
+  })
+  
+}
