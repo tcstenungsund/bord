d9b002f4de626cf8ab67f3bfa0a1a3b4c480abe7
williamhansen03 <91659748+williamhansen03@users.noreply.github.com>
2022-12-19 13:31:35 +0100

    Nfc get information (#99)

--- request.js
+++ request.js
@@ -9,7 +9,7 @@ const localhostBase = "http://localhost:8080";
 fruitBtn.addEventListener("click", function () {
   let user = "fruit";
   let card = "4a:2c:74:1b";
-  fetch(`${localhostBase}/${user}/${card}`, {
+  fetch(`${hostingBase}/${user}/${card}`, {
     method: "GET",
     headers: {
       "Content-Type": "text/html",
@@ -31,10 +31,9 @@ async function starRequest(){
   }
 
   if(idInput !== undefined){
-    fruitBtn.style.backgroundColor = "blue";
     try{
       let user = "fruit";
-      let card = "4a:2c:74:1b";
+      let card = idInput;
       fetch(`${hostingBase}/${user}/${card}`, {
         method: "GET",
         headers: {
